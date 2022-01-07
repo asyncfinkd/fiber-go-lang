f5ba2ce538bee83d6a51ba365fc9c31378b5c614
asyncfinkd <shamgoingzuck@gmail.com>
2022-01-07 23:16:30 +0400

    GET/POST Request for Todo Application

--- main.go
+++ main.go
@@ -2,6 +2,41 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 
+type Todo struct {
+	Id        int
+	text      string
+	completed bool
+}
+
+func getTodos(ctx *fiber.Ctx) error {
+	item := Todo{
+		1,
+		"Hello, World",
+		false,
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(item)
+}
+
+var todo Todo
+
+func createTodo(ctx *fiber.Ctx) error {
+	body := new(Todo)
+	err := ctx.BodyParser(body)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return err
+	}
+
+	todo = Todo{
+		Id:        body.Id,
+		text:      body.text,
+		completed: false,
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(todo)
+}
+
 func main() {
 	app := fiber.New()
 
@@ -9,5 +44,8 @@ func main() {
 		return ctx.SendString("Hello, World")
 	})
 
-	app.Listen(":8080")
+	app.Get("/todos", getTodos)
+	app.Post("add/todo", createTodo)
+
+	app.Listen(":80")
 }
